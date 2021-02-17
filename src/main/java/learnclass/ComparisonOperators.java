e7647c63b3334e3cedfd95982b32e0e7826bddf4
Hana Dawson <stubbornunicorn95@gmail.com>
2021-02-16 20:02:38 -0700

    boolean, for, break, and other basics.

---
+++ src/main/java/learnclass/ComparisonOperators.java
@@ -0,0 +1,15 @@
+package learnclass;
+
+public class ComparisonOperators {
+
+  public static void main(String[] args) {
+
+    int a = 5;
+    int b = 4;
+
+    boolean c = (a >= b);
+
+    System.out.println(c);
+  }
+
+}
